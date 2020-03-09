c04a3c328b683b22afda9924bf280f87e79574c5
Arthur Milchior <Arthur@Milchior.fr>
2020-03-09 11:04:07 +0100

    pep8 and sort

--- __init__.py
+++ __init__.py
@@ -1,19 +1,24 @@
+from anki.find import Finder
 from anki.hooks import addHook
 from aqt import mw
-from .config import getIntervalCoefficient, getIntervalForNegativeCoefficient
 from aqt.qt import QAction
-from aqt.utils import getText, tooltip, showWarning
-from anki.find import Finder
+from aqt.utils import getText, showWarning, tooltip
 
-#From https://stackoverflow.com/questions/1265665/how-can-i-check-if-a-string-represents-an-int-without-using-try-except
+from .config import getIntervalCoefficient, getIntervalForNegativeCoefficient
+
+# From https://stackoverflow.com/questions/1265665/how-can-i-check-if-a-string-represents-an-int-without-using-try-except
+
+
 def RepresentsInt(s):
-    try: 
+    try:
         return int(s)
     except ValueError:
         return None
 
+
 def getDelay():
     return getDelayWithResponse()[0]
+
 
 def getDelayWithResponse():
     (s, r) = getText("How many day to add to cards ? (negative number to substract days)")
@@ -21,10 +26,12 @@ def getDelayWithResponse():
         return (RepresentsInt(s), r)
     return (None, r)
 
+
 def getReviewCards():
     finder = Finder(mw.col)
     cids = finder.findCards("is:review")
     return cids
+
 
 def addDelay(cids):
     (delay, delayResp) = getDelayWithResponse()
@@ -36,13 +43,14 @@ def addDelay(cids):
     mw.checkpoint("Adding delay")
     mw.progress.start()
 
-    ivlDelay = max(0, round(delay * (getIntervalCoefficient() if delay >0 else getIntervalForNegativeCoefficient())))
+    ivlDelay = max(0, round(delay * (getIntervalCoefficient()
+                                     if delay > 0 else getIntervalForNegativeCoefficient())))
     for cid in cids:
         card = mw.col.getCard(cid)
-        if card.type !=2:
+        if card.type != 2:
             continue
         card.ivl += ivlDelay
-        if card.odid: # Also update cards in filtered decks
+        if card.odid:  # Also update cards in filtered decks
             card.odue += delay
         else:
             card.due += delay
@@ -54,19 +62,25 @@ def addDelay(cids):
 
     tooltip(_("""Delay added."""))
 
+
 def runMain():
     addDelay(getReviewCards())
-    
+
+
 def runBrowser(browser):
-    cids=browser.selectedCards()
+    cids = browser.selectedCards()
     addDelay(cids)
 
+
 text = "Postpone cards"
+
+
 def setupBrowserMenu(browser):
     a = QAction(text, browser)
-    a.triggered.connect(lambda : runBrowser(browser))
+    a.triggered.connect(lambda: runBrowser(browser))
     browser.form.menuEdit.addAction(a)
-    
+
+
 addHook("browser.setupMenus", setupBrowserMenu)
 action = QAction(mw)
 action.setText(text)
--- config.py
+++ config.py
@@ -1,27 +1,33 @@
 from aqt import mw
 
 userOption = None
+
+
 def getUserOption():
     global userOption
     if userOption is None:
         userOption = mw.addonManager.getConfig(__name__)
     return userOption
 
+
 def update(_):
     global userOption
     userOption = None
 
-mw.addonManager.setConfigUpdatedAction(__name__,update)
+
+mw.addonManager.setConfigUpdatedAction(__name__, update)
+
 
 def getIntervalCoefficient():
     return getUserOption().get("interval coefficient", 0.33)
 
+
 def getIntervalForNegativeCoefficient():
-    neg =  getUserOption().get("coefficient for negative",False)
+    neg = getUserOption().get("coefficient for negative", False)
     if neg is True:
         return getIntervalCoefficient()
     if neg is False:
         return 0
-    if isinstance(neg,(int,float)):
+    if isinstance(neg, (int, float)):
         return neg
     assert False
